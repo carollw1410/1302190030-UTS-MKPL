aeefecbbb9ce63c16de9c263eb7562c4580c9b66
carollw1410 <misspurple.carol14@gmail.com>
2022-04-21 00:18:46 +0800

    LongMethod-TaxFunction
    Membuat function baru.

--- TaxCalculator/src/lib/TaxFunction.java
+++ TaxCalculator/src/lib/TaxFunction.java
@@ -2,7 +2,6 @@ package lib;
 
 public class TaxFunction {
 
-	
 	/**
 	 * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
 	 * 
@@ -14,31 +13,32 @@ public class TaxFunction {
 	 * 
 	 */
 	
-	
 	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
 		
 		int tax = 0;
-		
-		if (numberOfMonthWorking > 12) {
+		cekNumberOfMonthWorking(int numberOfMonthWorking);
+		cekNumberOfChildren(int numberOfChildren);
+
+		public static void cekNumberOfMonthWorking(int numberOfMonthWorking){
+		if (numberOfMonthWorking > 12) 
 			System.err.println("More than 12 month working per year");
 		}
-		
-		if (numberOfChildren > 3) {
+		cekNumberOfChildren(int numberOfChildren){
+		if (numberOfChildren > 3) 
 			numberOfChildren = 3;
 		}
-		
+		public static boolean isMarried{
 		if (isMarried) {
 			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
 		}else {
 			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
 		}
-		
-		if (tax < 0) {
-			return 0;
-		}else {
-			return tax;
-		}
-			 
 	}
-	
+		public static int checkTax(int tax){
+			if (tax < 0) {
+				return 0;
+			}else {
+				return tax;
+			}		 
+	}
 }
